0f97deb6a8cdcbb0f72d8f24785b083cbcff5eae
ramsuthar305 <ramsuthar305@gmail.com>
2020-05-06 02:47:29 +0530

    intro slider done

--- App.js
+++ App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {
   View,
   Text,
@@ -8,32 +8,55 @@ import {
   SafeAreaView,
 } from 'react-native';
 import AppIntroSlider from 'react-native-app-intro-slider';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+const fetchFonts = () => {
+  console.log('loading font')
+  return Font.loadAsync({
+    // 'Heebo-regular': {
+    //   uri: require('./assets/fonts/Heebo-Regular.ttf'),
+    //   fontDisplay: FontDisplay.FALLBACK,
+    // },
+    'Nunito-regular': require('./assets/fonts/Nunito-Regular.ttf'),
+    'Nunito-Bold': require('./assets/fonts/Nunito-Bold.ttf'),
+    // 'Heebo-Bold': {
+    //   uri: require('./assets/fonts/Heebo-Bold.ttf'),
+    //   fontDisplay: FontDisplay.FALLBACK,
+    // },
+    // 'Heebo-Medium': {
+    //   uri: require('./assets/fonts/Heebo-Medium.ttf'),
+    //   fontDisplay: FontDisplay.FALLBACK,
+    // },
+  });
+};
+
 
 const data = [
   {
     key: 1,
-    title: 'Stay home, stay safe!\nHelp corona warriors by not stepping out of your home.',
-    text: 'Description.\nSay something cool',
+    title: 'Stay home, stay safe! \nHelp corona warriors by not stepping out of your home.',
+    text: 'Description.Say something cool',
     image: require('./assets/images/stay_home.jpg'),
     backgroundColor: '#59b2ab',
   },
   {
     key: 2,
-    title: 'finally!',
+    title: 'Social distancing is an essential way to slow down the spread of COVID-19!\nMaintain atleat 6 Feet distance',
     text: 'Other cool stuff',
     image: require('./assets/images/social_distance.jpg'),
     backgroundColor: '#febe29',
   },
   {
     key: 3,
-    title: 'Tribute to "jo jaa chuke j"',
+    title: 'Corona virus spread through nose, month and eyes. Prevent entry of virus by wearing mask',
     text: '',
     image: require('./assets/images/wear_mask.jpg'),
     backgroundColor: '#22bcb5',
   },
   {
     key: 4,
-    title: 'Tribute to "jo jaa chuke j"',
+    title: 'Germs can spread from other people or surfaces. Washing hands can  prevent the spread of respiratory and diarrheal infections',
     text: '',
     image: require('./assets/images/wash_hands.jpg'),
     backgroundColor: '#22bcb5',
@@ -53,14 +76,26 @@ const styles = StyleSheet.create({
   },
   title: {
     fontSize: 20,
-    color: 'black',
+    paddingHorizontal: "11%",
+    color: '#34495e',
     textAlign: 'center',
-    top: "70%",
-    marginTop:"5%"
+    top: "60%",
+    marginTop: "3%",
+    fontFamily: 'Nunito-Bold'
   },
 });
 
 export default function App() {
+  const [dataLoaded, setDataLoaded] = useState(false)
+
+  if (!dataLoaded) {
+    return (
+      <AppLoading
+        startAsync={fetchFonts}
+        onFinish={() => setDataLoaded(true)}
+      />
+    );
+  }
   _renderItem = ({ item }) => {
     return (
       <View
@@ -84,11 +119,10 @@ export default function App() {
     <View style={{ flex: 1 }}>
       <StatusBar translucent color="black" backgroundColor="black" />
       <AppIntroSlider
-        dotStyle={{backgroundColor:"grey"}}
-        activeDotStyle={{backgroundColor:"black"}}
+        dotStyle={{ backgroundColor: "grey" }}
+        activeDotStyle={{ backgroundColor: "black" }}
         keyExtractor={_keyExtractor}
         renderItem={_renderItem}
-        skipLabel="Skip"
         showSkipButton
         showPrevButton
         data={data}
